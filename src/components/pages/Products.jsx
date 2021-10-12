db15fd7d129b7c52ca33720f82c49c7f7eb7b8e0
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2021-10-12 22:09:04 +0300

    Transform data example (callback)

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
-const fetchProducts = () => axios.get('http://localhost:4000/poducts');
-const onSuccess = (data) => console.log(data.data.length);
+const fetchProducts = () => axios.get('http://localhost:4000/products');
+const onSuccess = (data) => console.log(data.length); // select affects the way onSuccess works
 const onError = (error) => console.log(error.message);
+const select = ({ data }) => {
+	const productNamesArray = data.map((product) => product.product_name);
+	return productNamesArray;
+};
 
 const Products = () => {
 	const { data, error, isLoading, refetch, isFetching } = useQuery('products', fetchProducts, {
@@ -20,6 +24,7 @@ const Products = () => {
 		// Callbacks
 		onSuccess,
 		onError,
+		select, // Transform data example
 	});
 
 	return (
@@ -28,8 +33,12 @@ const Products = () => {
 			{(isLoading || isFetching) && <div>Loading ...</div>}
 			{error && <div>{error.message}</div>}
 
-			{data?.data &&
-				data.data.map((product) => <h3 key={product._id.$oid}>{product.product_name}</h3>)}
+			{/*data?.data &&
+				data.data.map((product) => <h3 key={product._id.$oid}>{product.product_name}</h3>)*/}
+			{/** TRANSFORM: Possible only because of select callback */}
+			{data?.map((productName) => (
+				<div key={productName}>{productName}</div>
+			))}
 			<button onClick={() => refetch()} disabled={isFetching}>
 				Fetch Data On Click
 			</button>
