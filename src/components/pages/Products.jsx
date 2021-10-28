4a7c2513d4b97de2189e20a984bcfe4f5e63cfd9
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2021-10-28 21:43:52 +0300

    Implements useMutation hook

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useFetchData } from '../../customHooks/useFetchData';
+import React, { useState } from 'react';
+import { useFetchData, useCreateProduct } from '../../customHooks/useFetchData';
 import { Link } from 'react-router-dom';
 const onSuccess = (data) => console.log(data.length); // select affects the way onSuccess works
 const onError = (error) => console.log(error.message);
@@ -27,8 +27,31 @@ const options = {
 	queryId: 'products',
 };
 
+const product = {
+	product_name: 'product name',
+	supplier: 'supplier',
+	quantity: 0,
+	unit_cost: '$100',
+};
+
 const Products = () => {
 	const { data, error, isLoading, refetch, isFetching } = useFetchData(options);
+	const [prod, setProd] = useState(product);
+	const { mutate, isError, isLoading: loading } = useCreateProduct();
+
+	const onChangeHandler = ({ target }) => {
+		setProd((prod) => {
+			return { ...prod, [target.name]: target.value };
+		});
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		let id = Math.floor(((Math.random() * 205) / Math.random()) * 10 * 100);
+		mutate({ ...prod, id });
+
+		setProd(product);
+	};
 
 	return (
 		<div>
@@ -36,6 +59,51 @@ const Products = () => {
 
 			{(isLoading || isFetching) && <div>Loading ...</div>}
 			{error && <div>{error.message}</div>}
+			<div>
+				{isError && <p>Something Went Wrong</p>}
+				{loading && <p>Sending ...</p>}
+				<form>
+					<div className="form-control">
+						<label>Product Name</label>
+						<input
+							type="text"
+							name="product_name"
+							value={prod.product_name}
+							onChange={(event) => onChangeHandler(event)}
+						/>
+					</div>
+					<div className="form-control">
+						<label>Supplier</label>
+						<input
+							type="text"
+							name="supplier"
+							value={prod.supplier}
+							onChange={(event) => onChangeHandler(event)}
+						/>
+					</div>
+					<div className="form-control">
+						<label>Quantity</label>
+						<input
+							type="number"
+							name="quantity"
+							value={prod.quantity}
+							onChange={(event) => onChangeHandler(event)}
+						/>
+					</div>
+					<div className="form-control">
+						<label>Unit Cost</label>
+						<input
+							type="text"
+							name="unit_cost"
+							value={prod.unit_cost}
+							onChange={(event) => onChangeHandler(event)}
+						/>
+					</div>
+				</form>
+				<button type="submit" onClick={onSubmit}>
+					Send
+				</button>
+			</div>
 
 			{data?.data &&
 				data.data.map((product) => (
--- src/customHooks/useFetchData.js
+++ src/customHooks/useFetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 const fetchData = async (url) => {
 	return await axios.get(url);
@@ -11,4 +11,13 @@ export const useFetchData = (args) => {
 	return useQuery(queryId, () => fetchData(url), {
 		...rest,
 	});
+};
+
+/** Mutation Section */
+const createProduct = async (product) => {
+	return axios.post('http://localhost:4000/products', product);
+};
+
+export const useCreateProduct = () => {
+	return useMutation(createProduct);
 };
